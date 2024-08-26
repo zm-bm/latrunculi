f7add7e146446a0d966b5d285264841bcde4dcb5
Eric <vanderhelm.eric@gmail.com>
2024-08-26 00:24:35 -0500

    fix indent

--- include/types.hpp
+++ include/types.hpp
@@ -18,16 +18,16 @@ using I8 = int8_t;
 enum Color : U8 { BLACK, WHITE, NCOLORS = 2 };
 
 enum Square : U8 {
-    A1, B1, C1, D1, E1, F1, G1, H1,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A8, B8, C8, D8, E8, F8, G8, H8,
-    INVALID = 64,
-    NSQUARES = 64
+  A1, B1, C1, D1, E1, F1, G1, H1,
+  A2, B2, C2, D2, E2, F2, G2, H2,
+  A3, B3, C3, D3, E3, F3, G3, H3,
+  A4, B4, C4, D4, E4, F4, G4, H4,
+  A5, B5, C5, D5, E5, F5, G5, H5,
+  A6, B6, C6, D6, E6, F6, G6, H6,
+  A7, B7, C7, D7, E7, F7, G7, H7,
+  A8, B8, C8, D8, E8, F8, G8, H8,
+  INVALID = 64,
+  NSQUARES = 64
 };
 
 enum File : I8 { FILE1, FILE2, FILE3, FILE4, FILE5, FILE6, FILE7, FILE8 };
@@ -47,9 +47,9 @@ enum PieceType : U8 {
 };
 
 enum Piece : U8 {
-    EMPTY =  0,
-    BPAWN = 1, BKNIGHT, BBISHOP, BROOK, BQUEEN, BKING,
-    WPAWN = 9, WKNIGHT, WBISHOP, WROOK, WQUEEN, WKING,
+  EMPTY =  0,
+  BPAWN = 1, BKNIGHT, BBISHOP, BROOK, BQUEEN, BKING,
+  WPAWN = 9, WKNIGHT, WBISHOP, WROOK, WQUEEN, WKING,
 };
 
 enum CastleRights : U8 {
