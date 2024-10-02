fa736f1354729cf519498e95e38234ef2fedbc43
Eric VanderHelm <vanderhelm.eric@gmail.com>
2024-10-01 20:34:04 -0500

    remove prev BB impl

--- tests/board.test.cpp
+++ tests/board.test.cpp
@@ -15,12 +15,16 @@ TEST_F(BoardTest, BoardToFEN) {
   Board board(G::STARTFEN);
   EXPECT_EQ(board.toFEN(), G::STARTFEN);
 
-  board = Board(G::TESTFENS[0]);
-  EXPECT_EQ(board.toFEN(), G::TESTFENS[0]);
+  auto fen =
+    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0";
+  board = Board(fen);
+  EXPECT_EQ(board.toFEN(), fen);
 }
 
 TEST_F(BoardTest, Accessors) {
-  Board board(G::TESTFENS[2]);
+  Board board(
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+  );
   EXPECT_EQ(board.sideToMove(), WHITE);
   EXPECT_FALSE(board.canCastle(WHITE));
   EXPECT_TRUE(board.canCastle(BLACK));
@@ -29,12 +33,16 @@ TEST_F(BoardTest, Accessors) {
   EXPECT_EQ(board.getHmClock(), 0);
   EXPECT_EQ(board.getEnPassant(), INVALID);
 
-  board = Board(G::TESTFENS[3]);
+  board = Board(
+    "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"
+  );
   EXPECT_EQ(board.sideToMove(), BLACK);
   EXPECT_TRUE(board.canCastle(WHITE));
   EXPECT_FALSE(board.canCastle(BLACK));
 
-  board = Board(G::TESTFENS[4]);
+  board = Board(
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
+  );
   EXPECT_EQ(board.getHmClock(), 1);
 }
 
