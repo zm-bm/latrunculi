94d42e5612cab851c81d96cebbc1ae6117dcce25
Eric VanderHelm <vanderhelm.eric@gmail.com>
2024-10-01 19:44:30 -0500

    bitboard tests

--- tests/bitboard.test.cpp
+++ tests/bitboard.test.cpp
@@ -5,19 +5,13 @@
 #include "types.hpp"
 
 TEST(BitboardTest, clear) {
-  auto bb = BBz(0x1);
-  bb.clear(A1);
-  EXPECT_EQ(bb, BBz(0x0));
+  U64 bb = 0x1;
+  bb &= BB::clear(A1);
+  EXPECT_EQ(bb, 0x0);
 }
 
-TEST(BitboardTest, toggleSquare) {
-  auto bb = BBz(0x0);
-  bb.toggle(A1);
-  EXPECT_EQ(bb, BBz(0x1));
-}
-
-TEST(BitboardTest, toggleBitboard) {
-  auto bb = BBz(0x0);
-  bb.toggle(0x1);
-  EXPECT_EQ(bb, BBz(0x1));
+TEST(BitboardTest, set) {
+  U64 bb = 0x0;
+  bb ^= BB::set(A1);
+  EXPECT_EQ(bb, BB::set(A1));
 }
