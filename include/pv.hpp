a4c157a89b3f6663de5621147000707aa4e1aaaf
Eric VanderHelm <vanderhelm.eric@gmail.com>
2025-05-23 20:17:04 -0500

    refactor(pv): re-org PrincipalVariation

--- include/pv.hpp
+++ include/pv.hpp
@@ -9,35 +9,50 @@
 
 using MoveLine = std::vector<Move>;
 
-struct PrincipalVariation {
+class PrincipalVariation {
+   private:
     std::array<MoveLine, MAX_DEPTH> lines;
 
-    MoveLine bestLine() const { return lines[0]; }
-    Move bestMove(int ply = 0) const { return lines[ply].empty() ? NullMove : lines[ply][0]; }
-    void clear(int ply) { lines[ply].clear(); }
-
-    void update(const int ply, const Move& move) {
-        MoveLine& line = lines[ply];
-        MoveLine& prev = lines[ply + 1];
-
-        line.clear();
-        line.push_back(move);
-        line.insert(line.end(), prev.begin(), prev.end());
-    }
-
-    void clear() {
-        for (auto& line : lines) {
-            line.clear();
-        }
-    }
-
-    MoveLine& operator[](const int ply) { return lines[ply]; }
-
-    operator std::string() const {
-        std::string string;
-        for (const auto& move : lines[0]) {
-            string += move.str() + " ";
-        }
-        return string;
-    }
+   public:
+    void update(const int ply, const Move& move);
+    void clear(int ply);
+    void clear();
+    Move bestMove(int ply = 0) const;
+    MoveLine bestLine() const;
+    MoveLine& operator[](const int ply);
+    operator std::string() const;
 };
+
+inline void PrincipalVariation::update(const int ply, const Move& move) {
+    MoveLine& line = lines[ply];
+    MoveLine& prev = lines[ply + 1];
+
+    line.clear();
+    line.push_back(move);
+    line.insert(line.end(), prev.begin(), prev.end());
+}
+
+inline void PrincipalVariation::clear() {
+    for (auto& line : lines) {
+        line.clear();
+    }
+}
+
+inline void PrincipalVariation::clear(int ply) { lines[ply].clear(); }
+
+inline Move PrincipalVariation::bestMove(int ply) const {
+    auto front = lines[ply].front();
+    return lines[ply].empty() ? NullMove : lines[ply][0];
+}
+
+inline MoveLine PrincipalVariation::bestLine() const { return lines[0]; }
+
+inline MoveLine& PrincipalVariation::operator[](const int ply) { return lines[ply]; }
+
+inline PrincipalVariation::operator std::string() const {
+    std::string string;
+    for (const auto& move : lines[0]) {
+        string += move.str() + " ";
+    }
+    return string;
+}
