90ebaff81ab1c066155fdfc7ce8b8550cc7dd068
Eric VanderHelm <vanderhelm.eric@gmail.com>
2025-06-10 21:05:26 -0500

    feat(search): add nodes + movestogo to go cmd options

--- include/search_options.hpp
+++ include/search_options.hpp
@@ -15,14 +15,14 @@ struct ParseWarning {
 struct SearchOptions {
     std::string fen = STARTFEN;
     bool debug      = DEFAULT_DEBUG;
-
-    int depth    = MAX_DEPTH;
-    int movetime = INT32_MAX;
-
-    int wtime = TIME_NOT_SET;
-    int btime = TIME_NOT_SET;
-    int winc  = TIME_NOT_SET;
-    int binc  = TIME_NOT_SET;
+    int depth       = MAX_DEPTH;
+    int movetime    = INT32_MAX;
+    int nodes       = INT32_MAX;
+    int wtime       = TIME_NOT_SET;
+    int btime       = TIME_NOT_SET;
+    int winc        = 0;
+    int binc        = 0;
+    int movestogo   = 30;
 
     std::vector<ParseWarning> warnings;
 
@@ -40,6 +40,10 @@ struct SearchOptions {
                 max    = MAX_DEPTH;
             } else if (token == "movetime") {
                 target = &movetime;
+                min    = 1;
+                max    = INT32_MAX;
+            } else if (token == "nodes") {
+                target = &nodes;
                 min    = 0;
                 max    = INT32_MAX;
             } else if (token == "wtime") {
@@ -57,6 +61,10 @@ struct SearchOptions {
             } else if (token == "binc") {
                 target = &binc;
                 min    = 0;
+                max    = INT32_MAX;
+            } else if (token == "movestogo") {
+                target = &movestogo;
+                min    = 1;
                 max    = INT32_MAX;
             }
 
