f1187be3e77000cf9ebf808244c8654d155f5f82
Eric VanderHelm <vanderhelm.eric@gmail.com>
2025-06-27 22:24:58 -0500

    test(magic): cleanup magic tests

--- tests/unit/magic.test.cpp
+++ tests/unit/magic.test.cpp
@@ -6,75 +6,52 @@
 #include "constants.hpp"
 #include "test_utils.hpp"
 
-TEST(Magic_getBishopAttacks, BoardCenterNoObstacles) {
-    U64 expectedAttacks = BB::set(B1, C2, D3, F5, G6, H7, A8, H1, G2, F3, D5, C6, B7, A8);
+struct MagicTest {
+    Square square;
+    U64 occupancy;
+    U64 expectedAttacks;
+};
 
-    EXPECT_EQ(Magic::getBishopAttacks(E4, 0), expectedAttacks)
-        << "should attack fully both diagonals";
+class MagicBishopAttacksTest : public ::testing::TestWithParam<MagicTest> {};
+
+TEST_P(MagicBishopAttacksTest, ComputesAttacks) {
+    auto param = GetParam();
+    EXPECT_EQ(Magic::getBishopAttacks(param.square, param.occupancy), param.expectedAttacks);
 }
 
-TEST(Magic_getBishopAttacks, BoardCenterBlocked) {
-    U64 occupancy       = BB::set(F5, D5, F3);
-    U64 expectedAttacks = BB::set(B1, C2, D3, F5, D5, F3);
+INSTANTIATE_TEST_SUITE_P(
+    BishopAttacks,
+    MagicBishopAttacksTest,
+    ::testing::Values(
+        // Not blocked
+        MagicTest{E4, 0, BB::set(B1, C2, D3, F5, G6, H7, A8, H1, G2, F3, D5, C6, B7, A8)},
+        // Partially blocked
+        MagicTest{E4, BB::set(F5, D5, F3), BB::set(B1, C2, D3, F5, D5, F3)},
+        // Fully blocked
+        MagicTest{E4, BB::set(F5, F3, D5, D3), BB::set(F5, F3, D5, D3)},
+        // Piece on board edge, not blocked
+        MagicTest{A1, 0, BB::set(B2, C3, D4, E5, F6, G7, H8)},
+        // Piece on board edge, blocked by one square
+        MagicTest{A1, BB::set(C3), BB::set(B2, C3)}));
 
-    EXPECT_EQ(Magic::getBishopAttacks(E4, occupancy), expectedAttacks)
-        << "should be partially blocked";
+class MagicRookAttacksTest : public ::testing::TestWithParam<MagicTest> {};
+
+TEST_P(MagicRookAttacksTest, ComputesAttacks) {
+    auto param = GetParam();
+    EXPECT_EQ(Magic::getRookAttacks(param.square, param.occupancy), param.expectedAttacks);
 }
 
-TEST(Magic_getBishopAttacks, BoardCenterSurrounded) {
-    U64 occupancy       = BB::set(F5, F3, D5, D3);
-    U64 expectedAttacks = BB::set(F5, F3, D5, D3);
-
-    EXPECT_EQ(Magic::getBishopAttacks(E4, occupancy), expectedAttacks) << "should be fully blocked";
-}
-
-TEST(Magic_getBishopAttacks, BoardEdgeNoObstacles) {
-    U64 expectedAttacks = BB::set(B2, C3, D4, E5, F6, G7, H8);
-
-    EXPECT_EQ(Magic::getBishopAttacks(A1, 0), expectedAttacks) << "should attack single diagonal";
-}
-
-TEST(Magic_getBishopAttacks, BoardEdgeWithObstacles) {
-    U64 occupancy       = BB::set(C3);
-    U64 expectedAttacks = BB::set(B2, C3);
-
-    EXPECT_EQ(Magic::getBishopAttacks(A1, occupancy), expectedAttacks)
-        << "should be blocked on single diagonal";
-}
-
-TEST(Magic_getRookAttacks, BoardCenterNoObstacles) {
-    U64 expectedAttacks = (BB::rankBB(Rank::R4) | BB::fileBB(File::F5)) ^ BB::set(E4);
-
-    EXPECT_EQ(Magic::getRookAttacks(E4, 0), expectedAttacks)
-        << "should attack fully both ranks and files";
-}
-
-TEST(Magic_getRookAttacks, BoardCenterBlocked) {
-    U64 occupancy       = BB::set(D4, E5, G4);
-    U64 expectedAttacks = BB::set(D4, E5, E3, E2, E1, F4, G4);
-
-    EXPECT_EQ(Magic::getRookAttacks(E4, occupancy), expectedAttacks)
-        << "should be partially blocked";
-}
-
-TEST(Magic_getRookAttacks, BoardCenterSurrounded) {
-    U64 occupancy       = BB::set(D4, E5, E3, F4);
-    U64 expectedAttacks = BB::set(D4, E5, E3, F4);
-
-    EXPECT_EQ(Magic::getRookAttacks(E4, occupancy), expectedAttacks) << "should be fully blocked";
-}
-
-TEST(Magic_getRookAttacks, BoardEdgeNoObstacles) {
-    U64 expectedAttacks = (BB::rankBB(Rank::R1) | BB::fileBB(File::F1)) ^ BB::set(A1);
-
-    EXPECT_EQ(Magic::getRookAttacks(A1, 0), expectedAttacks)
-        << "should attack fully both ranks and files";
-}
-
-TEST(Magic_getRookAttacks, BoardEdgeWithObstacles) {
-    U64 occupancy       = BB::set(A4, B1);
-    U64 expectedAttacks = BB::set(A2, A3, A4, B1);
-
-    EXPECT_EQ(Magic::getRookAttacks(A1, occupancy), expectedAttacks)
-        << "should be blocked on both ranks and files";
-}
+INSTANTIATE_TEST_SUITE_P(
+    RookAttacks,
+    MagicRookAttacksTest,
+    ::testing::Values(
+        // Not blocked
+        MagicTest{E4, 0, BB::set(E1, E2, E3, E5, E6, E7, E8, A4, B4, C4, D4, F4, G4, H4)},
+        // Partially blocked
+        MagicTest{E4, BB::set(D4, E5, G4), BB::set(D4, E5, E3, E2, E1, F4, G4)},
+        // Fully blocked
+        MagicTest{E4, BB::set(D4, E5, E3, F4), BB::set(D4, E5, E3, F4)},
+        // Piece on board edge, not blocked
+        MagicTest{A1, 0, BB::set(A2, A3, A4, A5, A6, A7, A8, B1, C1, D1, E1, F1, G1, H1)},
+        // Piece on board edge, blocked
+        MagicTest{A1, BB::set(A4, B1), BB::set(A2, A3, A4, B1)}));
