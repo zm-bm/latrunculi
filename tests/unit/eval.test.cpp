1f64c8045333db58bad28c4c86291837b336433a
Eric VanderHelm <vanderhelm.eric@gmail.com>
2025-07-03 16:43:31 -0500

    test(score): add tests for score ops

--- tests/unit/eval.test.cpp
+++ tests/unit/eval.test.cpp
@@ -10,6 +10,21 @@
 
 using Scores = Eval<>::Scores;
 
+// TEST(ScoreTest, TaperScoreMidgame) {
+//     int exp = Score{100, 50}.taper(PHASE_LIMIT);
+//     EXPECT_EQ(exp, 100) << "max phase tapers to midgame";
+// }
+
+// TEST(ScoreTest, TaperScoreBetween) {
+//     int exp = Score{100, 50}.taper(PHASE_LIMIT / 2);
+//     EXPECT_EQ(exp, 75) << "half phase tapers to average";
+// }
+
+// TEST(ScoreTest, TaperScoreEndgame) {
+//     int exp = Score{100, 50}.taper(0);
+//     EXPECT_EQ(exp, 50) << "0 phase tapers to endgame";
+// }
+
 class EvalTest : public ::testing::Test {
    protected:
     void testOutposts(const std::string& fen, U64 expectedWhite, U64 expectedBlack) {
