3d9d12b6d81932f736b48dd689195425ad2207e9
Eric VanderHelm <vanderhelm.eric@gmail.com>
2025-07-02 23:26:10 -0500

    test(base): add base tests for piece, mate, and tt scores

--- include/base.hpp
+++ include/base.hpp
@@ -4,19 +4,11 @@
 #include "score.hpp"
 #include "types.hpp"
 
+// --------------------------
+// Piece values and scores
+// --------------------------
+
 constexpr int pieceValue(PieceType pieceType) { return PIECE_VALUES[idx(pieceType)]; }
-
-constexpr bool isMateScore(int score) { return std::abs(score) > MATE_IN_MAX_PLY; }
-
-constexpr int mateDistance(int score) { return MATE_SCORE - std::abs(score); }
-
-constexpr int ttScore(int score, int ply) {
-    if (score >= MATE_IN_MAX_PLY)
-        score += ply;
-    else if (score <= MATE_IN_MAX_PLY)
-        score -= ply;
-    return score;
-}
 
 constexpr Score pieceScore(PieceType pt) {
     auto p = idx(pt);
@@ -29,8 +21,26 @@ constexpr Score pieceScore(PieceType pt, Color c) {
 }
 
 constexpr Score pieceSqScore(PieceType pt, Color c, Square sq) {
-    auto p = idx(pt) - 1;
-    sq     = SQUARE_MAP[c][sq];
-    Score score{PSQ_VALUES[p][idx(Phase::MidGame)][sq], PSQ_VALUES[p][idx(Phase::EndGame)][sq]};
+    auto p  = idx(pt) - 1;
+    auto mg = idx(Phase::MidGame);
+    auto eg = idx(Phase::EndGame);
+    sq      = SQUARE_MAP[c][sq];
+    Score score{PSQ_VALUES[p][mg][sq], PSQ_VALUES[p][eg][sq]};
     return (score * c * 2) - score;
+}
+
+// --------------------------
+// Mate and TT scores
+// --------------------------
+
+constexpr bool isMateScore(int score) { return std::abs(score) > MATE_IN_MAX_PLY; }
+
+constexpr int mateDistance(int score) { return MATE_SCORE - std::abs(score); }
+
+constexpr int ttScore(int score, int ply) {
+    if (score >= MATE_IN_MAX_PLY)
+        score += ply;
+    else if (score <= MATE_IN_MAX_PLY)
+        score -= ply;
+    return score;
 }
